571ba66adfebf4a03d01070acbff0f1a60b96d5e
Sheryl Hohman <SherylHohman@yahoo.com>
2022-01-21 10:53:43 -0800

    1.5-extra-1 Custom Component merges styles
    Create a custom component that encapsulates the shared styles
      of the repeated elements
    It should accept and merge with styles unique
      to each of the element/custom component
    
    ----
      On branch sh
      Changes to be committed:
     	modified:   src/exercise/05.extra-1.js
     	modified:   src/exercise/05.extra-2.js

--- src/exercise/05.extra-1.js
+++ src/exercise/05.extra-1.js
@@ -1,32 +1,54 @@
 // Styling
-// http://localhost:3000/isolated/final/05.js
+// http://localhost:3000/isolated/exercise/05.js
 
 import * as React from 'react'
 import '../box-styles.css'
 
+// 💰 Use the className for the size and style (backgroundColor, fontStyle) for the color and the font style
+// 💰 each of the elements should also have the "box" className applied
+
+// 🐨 add a className prop to each of these and apply the correct class names
+// 💰 Here are the available class names: box, box--large, box--medium, box--small
+
+// 🐨 add a style prop to each of them as well so their background color
+// matches what the text says it should be as well as `fontStyle: 'italic'`
+
+// 1. 💯 Create a custom component
+// Production deploy
+
+// Try to make a custom <Box /> component that renders a div, accepts all the props and merges the given style and className props with the shared values.
+
+// I should be able to use it like so:
+
+// <Box className="box--small" style={{backgroundColor: 'lightblue'}}>
+//   small lightblue box
+// </Box>
+
+const Box = props => {
+  return (
+    <div
+      className={'box ' + props.className}
+      style={{fontStyle: 'italic', ...props.style}}
+    >
+      {props.children}
+    </div>
+  )
+}
+
 const smallBox = (
-  <div
-    className="box box--small"
-    style={{fontStyle: 'italic', backgroundColor: 'lightblue'}}
-  >
+  <Box className="box--small" style={{backgroundColor: 'lightblue'}}>
     small lightblue box
-  </div>
+  </Box>
 )
 const mediumBox = (
-  <div
-    className="box box--medium"
-    style={{fontStyle: 'italic', backgroundColor: 'pink'}}
-  >
+  <Box className="box--medium" style={{backgroundColor: 'pink'}}>
     medium pink box
-  </div>
+  </Box>
 )
 const largeBox = (
-  <div
-    className="box box--large"
-    style={{fontStyle: 'italic', backgroundColor: 'orange'}}
-  >
+  <Box className="box--large" style={{backgroundColor: 'orange'}}>
     large orange box
-  </div>
+  </Box>
 )
 
 function App() {
