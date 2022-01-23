3bec9abff3fa1fcb72727d7feef05b4e189f1ae4
Sheryl Hohman <SherylHohman@yahoo.com>
2022-01-23 09:07:01 -0800

    s1 solve 1.6: Forms onsubmitHandler
    ----
      On branch sh
      Changes to be committed:
     	modified:   src/exercise/06.js

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -15,15 +15,79 @@ function UsernameForm({onSubmitUsername}) {
   // 💰 For example: event.target.elements[0].value
   // 🐨 Call `onSubmitUsername` with the value of the input
 
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    /* console.log('submitted', event, event.nativeEvent)
+     */
+    // technically event will be React's "SynthethicEvent"
+    //  (a performance thing)
+    //  to get the Actual DOM event, use event.nativeEvent.
+    //  but that should never be neccesary, as they 99/9% will be the SAME
+    //  so always just use event.
+
+    //  if console.log(event.target), the Chrom dev tools will print out
+    //    the HTML of that DOM node.
+    //    In this case, that would be the form element and its children
+    //  To instead print the (form) object properties and values list, use
+    //    console.dir(event.target)
+    //  (console.log will work as expected for all other (
+    //    NON-HTML / Non DOM Node objects,
+    //    it is just DOM nodes that behaves this way in Chrome dev tools
+    //  )
+    /*
+    console.log(event.target)
+    console.dir(event.target)
+    console.log(
+      event.target.elements.usernameFieldName,
+      event.target.elements.usernameInputID,
+      event.target.elements[0],
+    )
+    */
+    // can only send a single object per call
+    // all 3 below pull up the SAME exact object.
+    // It calls itself input#usernameInputID
+    // (id="usernameInputID" was placed in the <input> tag)
+    /*
+    console.dir(event.target.elements.usernameFieldName)
+    console.dir(event.target.elements.usernameInputID)
+    console.dir(event.target.elements[0])
+    */
+
+    // the text "value" of the input field can be referenced as
+    // event.target.elements[0].value
+    //    though this would change per form, depending on its elements
+    // event.target.elements.usernameInputID
+    //    this is the id I assigned to the input element
+    //    <input id="usernameInputID" />
+    // event.target.elements.usernameFieldName
+    //    this is the "name" property I assigned to the input field's value
+    //    <input name="usernameFieldName" />
+    // obviously, I would probably set BOTH name and id to "username"
+    //    but I wanted to distinguish the id, name, htmlfor, and label
+    //    So I could see the structure, and wanted to be able to figure out
+    //    where the [user Input] was stored, and figure out how to access it.
+    //    Turns out there are many options.
+    //    Not sure what happens if everything is called "username"
+    //      which would it actually look to? It matters not, because they
+    //      would all  be the same here. Though if different elements
+    //      all had username for ONE of those fields, how would it know?
+    //      Dunno, do not care.
+    //    I think usually one would reference the NAME property of the
+    //      html element in question.
+
+    onSubmitUsername(event.target.elements.usernameFieldName.value)
+  }
+
   // 🐨 add the onSubmit handler to the <form> below
 
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="usernameInputID">Username:</label>
+        <input id="usernameInputID" name="usernameFieldName" type="text" />
       </div>
       <button type="submit">Submit</button>
     </form>
