727731b33c700246f8f70bc5827ac44b9aa9c7ef
Sheryl Hohman <SherylHohman@yahoo.com>
2022-01-24 14:32:43 -0800

    update 1.6-extra-2 input validation
    Properly disable the onSubmit button: disabled={Boolean(errorMessage)}
    Delete the faux disabled button hack
    remove the useRef stuff from previous extra-credit-1.
        revert to using the event.focus.usernameInputID.value
        it was ok for introducing useRef, but it is a worse and messy solution.
    
    ----
      On branch sh
      Changes to be committed:
     	modified:   src/exercise/06.extra-2.js

--- src/exercise/06.extra-2.js
+++ src/exercise/06.extra-2.js
@@ -3,45 +3,38 @@
 
 import * as React from 'react'
 
-// Extra credit 1: using refs
-// create a ref for the input element
-// use that ref, instead of the input id, to obtain the user input data/value
-// [useRef docs](https://reactjs.org/docs/hooks-reference.html#useref)
+// use the onChange eventHandler for input validation
+// use the useState hook and input validation to set form error message
+// error message element should set attribute role='alert' for screen readers
+// Disable the onSubmit button by setting its "disabled" attribute.
+//   Rem to overtly convert to (errorMessage) to Boolean
 
 function UsernameForm({onSubmitUsername}) {
-  const usernameInputRef = React.useRef(null)
   const [errorMessage, setErrorMessage] = React.useState(null)
 
   const handleSubmit = event => {
     event.preventDefault()
-    if (errorMessage) {
-      // disable submit button
-    } else {
-      onSubmitUsername(usernameInputRef.current.value)
-    }
+    onSubmitUsername(event.target.usernameInputID.value)
   }
 
   const handleChange = event => {
     const value = event.target.value
-    const isValid = value === value.toLowerCase()
-    setErrorMessage(isValid ? null : 'Username must be lower case')
+    const isLowerCase = value === value.toLowerCase()
+    setErrorMessage(isLowerCase ? null : 'Username must be lower case')
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="usernameInputID">Username:</label>
-        <input
-          onChange={handleChange}
-          ref={usernameInputRef}
-          id="usernameInputID"
-          type="text"
-        />
+        <input onChange={handleChange} id="usernameInputID" type="text" />
         <p role="alert" style={{color: 'red'}}>
           <i>{errorMessage}</i>
         </p>
       </div>
-      <button type="submit">Submit</button>
+      <button disabled={Boolean(errorMessage)} type="submit">
+        Submit
+      </button>
     </form>
   )
 }
